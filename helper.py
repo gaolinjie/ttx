f0a4ea3d7f50021fce57b6d6877866bca9666d7c
gaolinjie <gaolinjie@gmail.com>
2016-06-01 17:56:17 +0800

    fix img bugs

--- helper.py
+++ helper.py
@@ -30,6 +30,7 @@ class Filters():
         self.jinja2.filters["reply_process"] = self.reply_process
         self.jinja2.filters["mp_content_process"] = self.mp_content_process
         self.jinja2.filters["tmall_content_process"] = self.tmall_content_process
+        self.jinja2.filters["img_content_process"] = self.img_content_process
         self.jinja2.filters["markdown"] = self.markdown
         return self.jinja2
 
@@ -167,6 +168,12 @@ class Filters():
         content = re.sub(r'http://faxian.smzdm.com/9kuai9/', r'/baicai', content)
         content = re.sub(r'http://www.smzdm.com/tag/%E6%AF%8F%E6%97%A5%E7%99%BD%E8%8F%9C/faxian/', r'/baicai', content)
         
+        return content
+
+    def img_content_process(self, content):
+        if None==content:
+            return
+        content = re.sub(r'.jpg_d250.jpg', r'.jpg_d480.jpg', content)
 
         return content
 
