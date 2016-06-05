ae69d2ef7341e5f9c794fa31ecd77d423c520989
gaolinjie <gaolinjie@gmail.com>
2016-06-05 20:49:09 +0800

    update spide5

--- ttxspider/ttxspider/spiders/ttx_spider.py
+++ ttxspider/ttxspider/spiders/ttx_spider.py
@@ -55,8 +55,8 @@ class TtxSipder(CrawlSpider) :
         smzdm_match = smzdm_pattern.search(response.url)
         if smzdm_match: 
             item['pid'] = smzdm_match.group(1)
-        item['title']  = response.xpath('//h1/a/em/text()').extract()
-        item['subtitle']  = response.xpath('//h1/a/em/span[@class="red"]/text()').extract()
+        item['title']  = response.xpath('//h1/em/text()').extract()
+        item['subtitle']  = response.xpath('//h1/em/span[@class="red"]/text()').extract()
         item['intro']  = response.xpath('//div[@class="baoliao-block"]/p[1]/text()').extract()
         item['content']  = response.xpath('//div[@class="item-box item-preferential"]').extract()
         item['img']  = response.xpath('//a[@class="pic-Box"]/img/@src').extract()
